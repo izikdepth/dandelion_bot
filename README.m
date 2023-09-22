b3acb39be758689aeaa46dbdbc3abca43b855aec
IZIK <84680298+izikdepth@users.noreply.github.com>
2023-09-22 13:55:09 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,5 @@
 # dandelion_bot
+To run the bot, for the repo and put the bot token , admin id and channel id (for alerting members whose dm are closed) after that run ```python3 DDLN.py``` on linux or ```python DDLN.py``` on windows  to start the bot.
+This bot is an faq bot that also receives emails from members of a discord and sends those emails to the dm of the admin of the project.
+It checks every two weeks for members who haven't given their emails to the bot and ask them for it.
+Members can use command "/stop" to stop receiving notifications to send emails.
