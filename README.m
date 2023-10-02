dda25472ff67b8ace0112290446cd7e8d5f12620
IZIK <84680298+izikdepth@users.noreply.github.com>
2023-10-02 15:05:28 +0100

    Update README.m

--- README.m
+++ README.m
@@ -1,16 +1,19 @@
-# dandelion_bot
+# dandelion_bot#
 To run the bot, for the repo and put the bot token , admin id and channel id (for alerting members whose dm are closed) after that run ```python3 DDLN.py``` on linux or ```python DDLN.py``` on windows  to start the bot.
 This bot is an faq bot that also receives emails from members of a discord and sends those emails to the dm of the admin of the project.
 It checks every two weeks for members who haven't given their emails to the bot and ask them for it.
 Members can use command "/stop" to stop receiving notifications to send emails.
 
+###installing dependencies###
+```pip install -r requirements.txt```
+
 ##serverstats_example.py
 the serverstats_example code is an example of how you can display the price of a coin/token over a discord channel.
 
-##serverstats.py
+##serverstats.py##
 is the code you'll use in a Cog to display the price/marketcap/hashrate of a coin /token over a  discord channel
 
-##how to do?
+##how to do?##
 To display the price of a token over a  channel in discord:
 1. Add the bot to your discord server 
 2. create a  discord voice channel and name it whatever you want
