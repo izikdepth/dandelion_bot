496e7b0e9b23e753edb54f81f291a2ef31964899
IZIK <84680298+izikdepth@users.noreply.github.com>
2023-10-06 11:56:01 +0100

    Update DDLN.py

--- DDLN.py
+++ DDLN.py
@@ -11,11 +11,6 @@ intents = discord.Intents.default()
 intents.members = True
 
 
-##setting up logging 
-# logger = logging.getLogger('discord')
-# handler = logging.FileHandler(filename='DDLN_logs.log', encoding='utf-8', mode='w')
-# handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-# logger.addHandler(handler)
 
 #set bot command prefix and initial bot
 bot = commands.Bot(command_prefix="/", intents=intents)
@@ -34,5 +29,5 @@ Cogs_list = [
 for Cog in Cogs_list:
     bot.load_extension(f'Cogs.{Cog}')
 
-bot.run(os.getenv('Your bot token')) 
+bot.run(os.getenv('DDLN_BOT')) 
 
